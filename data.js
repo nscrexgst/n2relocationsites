13d21a44b99f1453e0716a87efa64c1c9e7440ac
krquinton <nscrexgst.dotr@gmail.com>
2022-09-29 19:53:40 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,570 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bondoc-ville",
+      "name": "Bondoc Ville",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1273080653694425,
+        "pitch": 0.6487706253778374,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6332126911251201,
+          "pitch": 0.10819688767752744,
+          "rotation": 0,
+          "target": "1-bondoc-ville-2"
+        },
+        {
+          "yaw": 0.2185135852049669,
+          "pitch": 0.4949076023328338,
+          "rotation": 0,
+          "target": "2-bondoc-ville---street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bondoc-ville-2",
+      "name": "Bondoc Ville (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5897623155552303,
+        "pitch": 0.39658282831140745,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3392496446428943,
+          "pitch": 0.051439031959205295,
+          "rotation": 0,
+          "target": "0-bondoc-ville"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bondoc-ville---street",
+      "name": "Bondoc Ville - Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3524058388274813,
+        "pitch": 0.20658996216949177,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5853610678792283,
+          "pitch": 0.2758840157071418,
+          "rotation": 0,
+          "target": "3-bondoc-ville---unit"
+        },
+        {
+          "yaw": 2.0764291352420603,
+          "pitch": -0.33019384876496716,
+          "rotation": 0,
+          "target": "0-bondoc-ville"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bondoc-ville---unit",
+      "name": "Bondoc Ville - Unit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6797627476479331,
+        "pitch": 0.26929525664635534,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-northville",
+      "name": "Northville",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5086637025459559,
+        "pitch": 1.2330482058801309,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5114476785981275,
+          "pitch": 0.7871420123613291,
+          "rotation": 0,
+          "target": "5-northville-2"
+        },
+        {
+          "yaw": 0.5419033057196163,
+          "pitch": 1.1909867141777646,
+          "rotation": 0,
+          "target": "6-northville---street"
+        },
+        {
+          "yaw": 1.0195360987847017,
+          "pitch": 0.6716733035500866,
+          "rotation": 0,
+          "target": "8-northville---market"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-northville-2",
+      "name": "Northville (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.28240113673177625,
+        "pitch": 0.5933949708803716,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31784077130735255,
+          "pitch": 0.5108356553368782,
+          "rotation": 0,
+          "target": "6-northville---street"
+        },
+        {
+          "yaw": 2.9197221333637087,
+          "pitch": 0.10801339225283435,
+          "rotation": 0,
+          "target": "8-northville---market"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-northville---street",
+      "name": "Northville - Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08379442623651912,
+        "pitch": -0.028150538096957334,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9258356689870446,
+          "pitch": 0.0554216896629427,
+          "rotation": 0,
+          "target": "7-northville---unit"
+        },
+        {
+          "yaw": 0.013595551142991269,
+          "pitch": -0.7015928771181432,
+          "rotation": 0,
+          "target": "5-northville-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-northville---unit",
+      "name": "Northville - Unit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4164608606426299,
+        "pitch": 0.2748110827441632,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.046113049579322,
+          "pitch": 0.08712848916133709,
+          "rotation": 0,
+          "target": "6-northville---street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-northville---market",
+      "name": "Northville - Market",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4742280122801823,
+        "pitch": 0.22319312663106672,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19672557025473836,
+          "pitch": -0.00048199331867238016,
+          "rotation": 0,
+          "target": "4-northville"
+        },
+        {
+          "yaw": -0.019631173368424015,
+          "pitch": 0.33036102361729647,
+          "rotation": 0,
+          "target": "2-bondoc-ville---street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-san-felipe",
+      "name": "San Felipe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.28893776308582986,
+        "pitch": 0.28863527634536545,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04778998449687499,
+          "pitch": 0.33643054134867434,
+          "rotation": 0,
+          "target": "10-san-felipe-2"
+        },
+        {
+          "yaw": 0.4344419130114012,
+          "pitch": 0.2946656863208439,
+          "rotation": 0,
+          "target": "12-san-felipe---street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-san-felipe-2",
+      "name": "San Felipe (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2674739205218408,
+          "pitch": 0.3526316190753356,
+          "rotation": 0,
+          "target": "2-bondoc-ville---street"
+        },
+        {
+          "yaw": 0.4294075574772691,
+          "pitch": 0.27833811822218024,
+          "rotation": 0,
+          "target": "11-san-felipe---unit"
+        },
+        {
+          "yaw": 1.8614203785863923,
+          "pitch": -0.009614396648380819,
+          "rotation": 0,
+          "target": "9-san-felipe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-san-felipe---unit",
+      "name": "San Felipe - Unit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1555628053796276,
+          "pitch": 0.004924425224766438,
+          "rotation": 0,
+          "target": "12-san-felipe---street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-san-felipe---street",
+      "name": "San Felipe - Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.32269674941019666,
+        "pitch": 0.00007526338174912439,
+        "fov": 1.4037187550928973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.25051146242788747,
+          "pitch": 0.24993170393751818,
+          "rotation": 0,
+          "target": "11-san-felipe---unit"
+        },
+        {
+          "yaw": -0.05520541671972268,
+          "pitch": -0.2835159820221982,
+          "rotation": 0,
+          "target": "9-san-felipe"
+        },
+        {
+          "yaw": 0.9917999316036905,
+          "pitch": -0.23217137115512543,
+          "rotation": 0,
+          "target": "10-san-felipe-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "N2 Relocation Sites",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
